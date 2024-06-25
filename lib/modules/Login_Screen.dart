f32133b91707843fd768c6a8ca2b252b33c56f51
Faswet <minasafwat594@gmail.com>
2024-06-25 14:42:00 +0300

    inializtion project

--- lib/modules/Login_Screen.dart
+++ lib/modules/Login_Screen.dart
@@ -1,12 +1,120 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  
+  bool observebool= true;
+  var testfieldkey= GlobalKey<FormState>();
+  var arid= TextEditingController();
+  var password= TextEditingController();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
+      body: Form(
+        key: testfieldkey,
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'AVA REWASE VISIT',
+                style: TextStyle(
+                  fontSize: 30,
+
+                  fontWeight: FontWeight.bold
+                ),
+
+              ),
+              SizedBox(height: 60,),
+
+              TextFormField(
+                validator: (value) {
+                  if(value==null || value.isEmpty){
+                    return ' please enter your ARID!';
+                  }
+                  return null;
+                },
+                controller: arid,
+
+                decoration: InputDecoration(
+                  labelText: 'E1C1FXXXNRX',
+
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))
+                ),
+              ),
+              SizedBox(height: 20,),
+
+              TextFormField(
+                obscureText: observebool,
+                validator: (value) {
+                  if(value==null || value.isEmpty){
+                    return ' please enter your password';
+                  }
+                  return null;
+                },
+                controller: password,
+
+                decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                      onPressed: (){
+                        setState(() {
+                          observebool=!observebool;
+
+
+                        });
+                      } ,
+                      icon: observebool?Icon(Icons.visibility):Icon(Icons.visibility_off)
+                    ),
+                    labelText: 'Password',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10)
+                  )
+                ),
+              ),
+              SizedBox(height: 40,),
+
+              Container(
+                height: 49,
+                width: double.infinity,
+                child: MaterialButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)
+
+                  ),
+
+                    onPressed: (){
+                      if(testfieldkey.currentState!.validate()){
+                        print(arid.text);
+                        print(password.text);
+                      }
+
+                    },
+                  child: Text(
+                      'Login',
+                    style: TextStyle(
+                      color: Colors.white
+                    ),
+                  ),
+                  color: Colors.black,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
