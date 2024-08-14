f66f251cef96c7de8c68358f8c8cdc12f7b10c80
marcosherif <68437948+marcosherif@users.noreply.github.com>
2024-08-14 03:56:16 +0300

    ADDED firebase configuration to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:ar_visiting_app/modules/Login_Screen.dart';
 import 'package:ar_visiting_app/modules/New_Visits_Screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -12,11 +18,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       home: newVisitScreen(),
     );
   }
 }
-
