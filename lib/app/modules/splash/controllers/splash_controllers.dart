e8903553aafae7c7dec55da024a807bc46f25692
MinaSafwat2411 <102329342+Faswet12@users.noreply.github.com>
2025-02-06 21:02:38 +0200

    new application

--- lib/app/modules/splash/controllers/splash_controllers.dart
+++ lib/app/modules/splash/controllers/splash_controllers.dart
@@ -1,8 +1,9 @@
 
 import 'package:ar_visiting_app/app/core/services/dio_helper.dart';
-import 'package:ar_visiting_app/app/core/services/secure_cache_helper.dart';
 import 'package:ar_visiting_app/app/core/utils/backend_endpoint.dart';
 import 'package:get/get.dart';
+import '../../../core/services/cache_helper.dart';
+import '../../../core/services/secure_cache_helper.dart';
 import '../../../routes/app_pages.dart';
 
 class SplashController extends GetxController {
@@ -12,25 +13,28 @@ class SplashController extends GetxController {
 
   @override
   void onInit() async {
-    token.value = await SecureCacheHelper.getData(key: 'token') ?? '';
-    lang.value = await SecureCacheHelper.getData(key: 'lang') ?? 'en';
     super.onInit();
     onNavigate();
   }
 
+
   void onNavigate() async {
-    reload.value = false;
-    if (token.isNotEmpty) {
-      try {
-        await DioHelper.getData(
-            url: BackendEndpoint.enums, token: token.value,lang: lang.value);
-                  Get.offNamed(Routes.HOME);
-      } catch (e) {
-        Get.snackbar('Error', 'check your connection');
-        reload.value = true;
-      }
-    } else {
+    try {
+      token.value = await SecureCacheHelper.getData(key: 'token') ?? '';
+      lang.value = await CacheHelper.getData(key: 'lang') ?? 'en';
+
+      await DioHelper.getData(
+        url: BackendEndpoint.enums,
+        token: token.value,
+        lang: lang.value,
+      );
+      Get.offNamed(Routes.HOME);
+    } on NoInternetException catch (e) {
+      reload(true);
+      Get.snackbar('No Internet', "Check your internet connection");
+    } on Exception catch (e) {
       Get.offNamed(Routes.LOGIN);
     }
   }
+
 }
